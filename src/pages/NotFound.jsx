75051e78b1e25ab5ed91e9224bd1f343dab2322d
stateofjace512 <jakeryanrobison@icloud.com>
2025-09-12 16:20:55 -0500

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Navigation from "../components/Navigation";
-import Footer from '../components/Footer";
+import Footer from "../components/Footer";
 
 export default function NotFound() {
   const handleBack = (e) => {
